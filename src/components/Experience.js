7bda3e31dfb6393472956e332e08b6a563fe27c9
luciabrown <brownlu@tcd.ie>
2025-09-08 22:29:40 +0100

    Added js files

---
+++ src/components/Experience.js
@@ -0,0 +1,26 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Experience = () => {
+
+  return (
+    <section className="experience" id="experience">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Experience</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
