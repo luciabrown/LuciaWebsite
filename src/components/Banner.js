e012b14c152e7b5664bd76e9e6d8e9caaff60db2
luciabrown <brownlu@tcd.ie>
2025-09-08 21:31:18 +0100

    Simplifying the job

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/pinkFlower.svg";
+import headerImg from "../assets/img/me.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
